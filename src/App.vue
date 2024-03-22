27bc6b41eebed1244db4aacb5a2a7d58c663ec22
정성원 <swjung@itembay.com>
2024-03-22 16:22:55 +0900

    0322

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="menu">
+  <!-- 헤더 -->
+  <header class="menu">
     <div class="container">
       <a href="javascript:;" class="logo"><img src="./assets/logo-text.svg" alt="로고" /></a>
       <ul class="menu-list">
@@ -7,43 +8,80 @@
           <a href="javascript:;">{{ a }}</a>
         </li>
       </ul>
+      <ul>
+        <li><button type="button"><i class="fa-regular fa-lightbulb"></i></button></li>
+        <li><button type="button"></button></li>
+      </ul>
     </div>
-  </div>
+  </header>
+  <!-- 헤더 끝 -->
+
+  <!-- 게시글 영역 -->
   <div class="post-area">
     <div class="container">
       <!-- 
         (item, i)중 item은 postTitle 배열의 데이터
         i는 length값 (0 1 2 3 4 5~)
        -->
-      <div class="post-area-item" v-for="(item, i) in postTitle" :key="i">
-        <a href="javscript:">
-          <h4>{{ item }}</h4>
-          <p>{{ postContent[i] }}</p>
-          <!-- <button v-on:click="">추천</button><span>1</span> -->
-        </a>
-        <button @click="recommendAdd" class="btn_recommend">추천<span>{{ recommend[i] }}</span></button>
+       <div class="post-area-cont">
+        <div class="post-area-item" v-for="(list, i) in list" :key="i">
+          <div class="post-area-item__img">
+            <img :src="list.image" :alt="list.title" @click="modal = true; listModal = i" />
+          </div>
+          <a href="javscript:" class="post-area-item__text">
+            <h4>{{ list.title }}</h4>
+            <p>{{ list.content }}</p>
+            <!-- <button v-on:click="">추천</button><span>1</span> -->
+          </a>
+          <!-- <button @click="recommendAdd" class="btn_recommend">♡</button> -->
+          <button id="listFavorite" class="btn_recommend" @click="listFavorite">{{ list.favorite[0] }}</button>
+        </div>
+       </div>
+    </div>
+  </div>
+  <!-- 게시글 영역 끝 -->
+
+  <div class="list-modal" v-if="modal == true" @click="modal = false">
+    <div class="list-modal-item" @click.stop>
+      <div class="list-modal-item__img">
+        <img :src="list[listModal].image" :alt="list[listModal].title" />
       </div>
+      <h4>{{ list[listModal].title }}</h4>
+      <p>{{ list[listModal].content }}</p>
+      <button type="button" @click="modal = false">닫기</button>
     </div>
   </div>
 </template>
 
 <script>
 
+import listData from './assets/js/list.js';
+
 export default {
   name : 'App',
   data() {
     return {
       menus : ['Post', 'Location'],
-      postTitle : ['첫번째 게시글', '두번째 게시글'],
-      postContent : ['첫번째 글 내용', '두번째 글 내용'],
-      recommend : [0, 0],
+      list : listData,
+      listModal : 0,
+      // postTitle : ['첫번째 게시글', '두번째 게시글', '3번째 게시글', '4번째 게시글', '5번째 게시글', '6번째 게시글', '7번째 게시글', '8번째 게시글', '9번째 게시글'],
+      // postContent : ['첫번째 글 내용', '두번째 글 내용', '3번째 글 내용', '4번째 글 내용', '5번째 글 내용', '6번째 글 내용', '7번째 글 내용', '8번째 글 내용', '9번째 글 내용'],
+      // recommend : [0, 0],
+      modal : false,
     }
   },
   // 함수 만드는 공간
   methods : {
-    recommendAdd() {
-      this.recommen++;
-    },
+    listFavorite() {
+      // 클릭 시 list.favorite[0]를 list.favorite[1]로 변경
+      this.list.favorite[0] = this.list.favorite[1];
+      if(this.list.favorite[0]) {
+        this.list.favorite[1];
+      }
+    }
+    // recommendAdd() {
+    //   this.list.recommend++;
+    // },
   },
   components : {
   }
---
+++ src/assets/js/list.js
@@ -0,0 +1,42 @@
+export default [{
+  id : 0,
+  title: "1번째 게시글",
+  image: "https://codingapple1.github.io/vue/room0.jpg",
+  content: "1번째 글 설명",
+  favorite: ["♡","♥"]
+  },
+  {
+  id : 1,
+  title: "2번째 게시글",
+  image: "https://codingapple1.github.io/vue/room1.jpg",
+  content: "2번째 글 설명",
+  favorite: ["♡","♥"]
+  },
+  {
+  id : 2,
+  title: "3번째 게시글",
+  image: "https://codingapple1.github.io/vue/room2.jpg",
+  content: "3번째 글 설명",
+  favorite: ["♡","♥"]
+  },
+  {
+  id : 3,
+  title: "4번째 게시글",
+  image: "https://codingapple1.github.io/vue/room3.jpg",
+  content: "4번째 글 설명",
+  favorite: ["♡","♥"]
+  },
+  {
+  id : 4,
+  title: "5번째 게시글",
+  image: "https://codingapple1.github.io/vue/room4.jpg",
+  content: "5번째 글 설명",
+  favorite: ["♡","♥"]
+  },
+  {
+  id : 5,
+  title: "6번째 게시글",
+  image: "https://codingapple1.github.io/vue/room5.jpg",
+  content: "6번째 글 설명",
+  favorite: ["♡","♥"]
+}];
